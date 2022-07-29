9ef379ed731f58211a7f8a1091f6b3a53e26c59b
Andrzej Olszewski <andrzej.olszewski@comarch.com>
2022-07-29 14:33:08 +0200

    BSFI-28 Add user view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,7 +16,31 @@ export default {
 <template>
   <q-layout view="hHh LpR lff" container>
     <q-header elevated>
-      <NavBar />
+      <q-toolbar id="toolbar">
+        <q-btn
+          flat
+          @click="drawer = !drawer"
+          round
+          dense
+          icon="menu"
+          size="25px"
+        />
+
+        <q-btn flat dense :to="{ name: 'home' }" @click="link = 'home'">
+          <img src="@/assets/logo.svg" height="50" />
+        </q-btn>
+
+        <q-btn
+          flat
+          round
+          dense
+          icon="person"
+          size="25px"
+          :to="{ name: 'user' }"
+          @click="link = 'user'"
+          style="font-size: 20px"
+        />
+      </q-toolbar>
     </q-header>
 
     <q-drawer
@@ -145,6 +169,16 @@ export default {
 #content
   align-items: center
   display: flex
+
+#toolbar
+  left: 0px
+  top: 0px
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  align-items: center
+  padding: 0px 30px
+  background: $primary
 
 .selected-link
   color: black
--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -1,3 +1,132 @@
+<script setup>
+import { ref } from "vue";
+
+const state = ref({
+  disable: true,
+  isEdited: false,
+  name: "",
+  surname: "",
+  email: "",
+  password: "",
+  country: "",
+  city: "",
+  street: "",
+  streetNumber: "",
+  houseNumber: "",
+  zipCode: "",
+  postOffice: "",
+});
+
+const edit = () => {
+  state.value.disable = !state.value.disable;
+  state.value.isEdited = !state.value.isEdited;
+};
+</script>
+
 <template>
-  <p>Tu będzie widok do edycji usera</p>
+  <div>
+    <h3 class="row q-mx-md">Account</h3>
+    <div class="row q-gutter-xl">
+      <div class="col q-gutter-md">
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.name"
+          label="Name"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.surname"
+          label="Surname"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.email"
+          label="Email"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.password"
+          label="Password"
+          :disable="state.disable"
+        />
+      </div>
+      <div class="col q-gutter-md">
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.country"
+          label="Country"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.city"
+          label="City"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.street"
+          label="Street"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.streetNumber"
+          label="Street number"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.houseNumber"
+          label="House number"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.zipCode"
+          label="Zip code"
+          :disable="state.disable"
+        />
+        <q-input
+          class="text-input"
+          outlined
+          v-model="state.postOffice"
+          label="Post office"
+          :disable="state.disable"
+        />
+      </div>
+    </div>
+    <q-btn v-if="state.isEdited === false" class="buttons q-mx-md" @click="edit"
+      >Edit
+    </q-btn>
+    <q-btn v-if="state.isEdited === true" class="buttons q-mx-md" @click="edit">
+      Save
+    </q-btn>
+  </div>
 </template>
+
+<style scoped>
+.text-input {
+  width: 20rem;
+}
+
+.buttons {
+  font-size: large;
+  /* margin: 5% auto 0 auto;s */
+  color: white;
+  background-color: #1976d2;
+}
+</style>
