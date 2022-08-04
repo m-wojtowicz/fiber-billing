6bf42a6ac5667845c1cd484f8a953e0e9ebceb0f
Michał Wójtowicz <michal.wojtowicz@comarch.com>
2022-08-04 14:05:19 +0200

    BSFI-15 Updated OrderDetails window

--- frontend/src/components/OrderDetails.vue
+++ frontend/src/components/OrderDetails.vue
@@ -68,8 +68,56 @@ const products = ref([
       },
     ],
   },
-    {
+  {
     id: 2,
+    name: "Product #2",
+    state: "Activated",
+    cost: "49,99",
+    parameters: [
+      {
+        id: 1,
+        name: "parameter #1",
+      },
+      {
+        id: 2,
+        name: "parameter #2",
+      },
+      {
+        id: 3,
+        name: "parameter #3",
+      },
+      {
+        id: 4,
+        name: "parameter #4",
+      },
+      {
+        id: 5,
+        name: "parameter #5",
+      },
+      {
+        id: 6,
+        name: "parameter #6",
+      },
+      {
+        id: 7,
+        name: "parameter #7",
+      },
+      {
+        id: 8,
+        name: "parameter #8",
+      },
+      {
+        id: 9,
+        name: "parameter #9",
+      },
+      {
+        id: 10,
+        name: "parameter #10",
+      },
+    ],
+  },
+  {
+    id: 3,
     name: "Product #2",
     state: "Activated",
     cost: "49,99",
@@ -134,52 +182,58 @@ const totalcost = 4;
             order.creation.getMonth()
           }}-{{ order.creation.getFullYear() }}
         </div>
-        <div class="col text-h5 text-center text-bold">Order #{{ order.id }}</div>
+        <div class="col text-h5 text-center text-bold">
+          Order #{{ order.id }}
+        </div>
         <div class="col text-h6 text-right">State: {{ order.state }}</div>
       </div>
-      <q-card-section class="items-center q-pb-none">
-        <q-scroll-area class="q-gutter-y-md" style="height: 300px" visible>
-        <div class="q-gutter-y-md">
-                    <div
-            v-for="product in products"
-            :key="product.id"
-            class="product-section"
-          >
-            <div class="row">
-              <div class="col text-h6 text-left">
-                Product ID: {{ product.id }}
-              </div>
-              <div class="col text-h6 text-center">
-                {{ product.name }}
-              </div>
-              <div class="col text-h6 text-right">
-                State: {{ product.state }}
-              </div>
-            </div>
-            <div class="row justify-between">
-              <div class="col-9 parameters-section">
-                <q-scroll-area style="height: 80px" visible>
-                  <div class="text-left">
-                    <li
-                      v-for="parameter in product.parameters"
-                      :key="parameter.id"
-                    >
-                      {{ parameter.name }}
-                    </li>
+      <q-card-section
+        class="items-center q-pb-none column"
+        style="min-height: 450px"
+      >
+        <div class="col full-width" style="height: 120%">
+          <q-scroll-area visible style="height: 450px">
+            <div class="q-gutter-y-md">
+              <div
+                v-for="product in products"
+                :key="product.id"
+                class="product-section"
+              >
+                <div class="row">
+                  <div class="col text-h6 text-left">
+                    Product ID: {{ product.id }}
                   </div>
-                </q-scroll-area>
-              </div>
-              <div class="col text-h6">
-                {{ product.cost }} zł/mo
+                  <div class="col text-h6 text-center">
+                    {{ product.name }}
+                  </div>
+                  <div class="col text-h6 text-right">
+                    State: {{ product.state }}
+                  </div>
+                </div>
+                <div class="row justify-between">
+                  <div class="col-9 parameters-section">
+                    <q-scroll-area style="height: 80px" visible>
+                      <div class="text-left">
+                        <li
+                          v-for="parameter in product.parameters"
+                          :key="parameter.id"
+                        >
+                          {{ parameter.name }}
+                        </li>
+                      </div>
+                    </q-scroll-area>
+                  </div>
+                  <div class="col text-h6">{{ product.cost }} zł/mo</div>
+                </div>
               </div>
             </div>
-          </div>
-
+          </q-scroll-area>
         </div>
-        </q-scroll-area>
       </q-card-section>
-      <q-card-section class="items-center">
-        <div class="text-h5 text-center text-bold">Total: {{ totalcost }} zł/mo</div>
+      <q-card-section class="items-center" >
+        <div class="text-h5 text-center text-bold">
+          Total: {{ totalcost }} zł/mo
+        </div>
       </q-card-section>
     </q-card>
   </q-dialog>
@@ -223,6 +277,7 @@ const totalcost = 4;
   box-shadow: 4px 8px 4px rgba(0, 0, 0, 0.25)
   border-radius: 30px
   padding: 5px 0px 0px 0px
+  margin-bottom: 10px
 
 .parameters-section
   background: $second-bg
