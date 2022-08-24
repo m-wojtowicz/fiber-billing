7571e1a16d3af5193cfb3efdcd5b0452fabb04b5
Andrzej Olszewski <andrzej.olszewski@comarch.com>
2022-08-24 12:44:32 +0200

    BSFI-50 Final version of New order view

--- frontend/src/views/NewOrderView.vue
+++ frontend/src/views/NewOrderView.vue
@@ -1,29 +1,37 @@
 <script setup>
 import OfferCard from "../components/OfferCard.vue";
+
 let offers = [
   { name: "Offer", description: "asdasds asd" },
   { name: "Offer", description: "asdasds asd" },
 ];
 </script>
-<template class="flex items-start">
-  <div>
-    <div class="header flex justify-between q-ma-lg">
+
+<template>
+  <div id="new-order">
+    <div class="header flex justify-between q-my-lg q-mx-xl">
       <q-btn class="cart_button" icon="shopping_cart" />
       <h3>Avalible offers</h3>
       <p class="order-number">Order number: 1323</p>
     </div>
-    <ul class="q-gutter-xl">
-      <OfferCard v-for="offer in offers" :key="offer.name" class="q-mx-xl" />
-    </ul>
-    <div class="q-mt-lg">
-      <q-btn class="configure_button q-ma-md"> Configure Products </q-btn>
+    <div class="flex justify-center q-mt-xl">
+      <ul class="q-gutter-xl">
+        <OfferCard v-for="offer in offers" :key="offer.name" class="q-mx-xl" />
+      </ul>
+    </div>
+    <div class="q-mt-xl flex justify-end">
+      <q-btn class="configure_button q-mt-xl q-mr-xl">
+        Configure Products
+      </q-btn>
     </div>
   </div>
 </template>
 
 <style scoped lang="sass">
-.header
+#new-order
+  min-height: inherit
   width: 100%
+
 .cart_button
   background-color: #1976D2
   color: #FFFFFF
